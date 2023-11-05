4548cb9d10bec97f026ecefc2b74fb192b4ad2cd
DandinPower <tomhot246@gmail.com>
2023-11-05 21:08:53 +0800

    add: finished hw2 all features

---
+++ hw2/submit/zip.sh
@@ -0,0 +1,10 @@
+rm -f HW2_312581029.zip
+rm -rf part1 
+rm -rf part2 
+mkdir part1
+mkdir part2
+cp ../part1/Makefile part1
+cp ../part1/pi.c part1
+cp ../part2/mandelbrotThread.cpp part2
+
+zip HW2_312581029.zip part1/* part2/* url.txt
