753c470d3f39f1772fb2ea19a14d5d0c0f9fa334
DandinPower <tomhot246@gmail.com>
2023-11-24 19:09:03 +0800

    add: initialize hw4 and backup previous homework

--- hw3/submission/zip.sh
+++
@@ -1,9 +0,0 @@
-rm -f HW3_312581029.zip
-rm -f *.c
-rm -f *.cpp
-
-cp ../part1/cg_impl.c .
-cp ../part2/breadth_first_search/bfs.cpp .
-cp ../part2/page_rank/page_rank.cpp .
-
-zip HW3_312581029.zip *.c *.cpp
