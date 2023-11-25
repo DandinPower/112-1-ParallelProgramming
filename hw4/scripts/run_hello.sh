24715707b3d96df1208bb8ad42828fb1f17af7f9
DandinPower <tomhot246@gmail.com>
2023-11-25 21:41:12 +0800

    scripts: refactor for ssh host by parallel ssh/scp

--- hw4/scripts/run_hello.sh
+++ hw4/scripts/run_hello.sh
@@ -5,40 +5,24 @@ CURRENT_DIR=$(pwd)
 HOME_DIR=$HOME
 SUB_DIR=${CURRENT_DIR#$HOME_DIR/}
 
-# Function to copy files to remote host
-copy_to_host() {
-    local host=$1
-    ssh "$host" "mkdir -p ~/$SUB_DIR"
-    scp mpi_executable "$host:~/$SUB_DIR"
-}
-
-# Function to clean up files on remote host
-cleanup_on_host() {
-    local host=$1
-    ssh "$host" "rm -rf ~/$SUB_DIR"
-}
-
-# Create hosts file
-touch hosts
-for host in "${HOSTS[@]}"; do
-    echo "$host" >> hosts
-done
+# Create a hosts file
+printf "%s\n" "${HOSTS[@]}" > hosts
 
 # Compile executable
 cp ../src/$TARGET .
 mpicxx ./$TARGET -o mpi_executable
 
-# Copy the executable to each host
-for host in "${HOSTS[@]}"; do
-    copy_to_host "$host"
-done
+# Create directories in parallel on each host
+parallel-ssh -h hosts -i "mkdir -p ~/$SUB_DIR"
+
+# Copy the executable to each host in parallel
+parallel-scp -h hosts -r mpi_executable ~/$SUB_DIR
 
 # (Optional) Insert commands here that use the copied files on remote hosts
-mpirun -np 16 --hostfile hosts mpi_executable
+mpirun -np 16 --hostfile hosts ~/$SUB_DIR/mpi_executable
 
-# Clean up
-for host in "${HOSTS[@]}"; do
-    cleanup_on_host "$host"
-done
+# Clean up files on remote hosts in parallel
+parallel-ssh -h hosts -i "rm -rf ~/$SUB_DIR"
 
+# Local clean up
 rm $TARGET mpi_executable hosts
