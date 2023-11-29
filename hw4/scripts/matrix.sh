dfea9c8efd96d3ec638935aa8fb9d8d1a8dd5f53
DandinPower <tomhot246@gmail.com>
2023-11-29 17:12:41 +0800

    add: finished easy version of hw4 part2

---
+++ hw4/scripts/matrix.sh
@@ -0,0 +1,61 @@
+# Define hosts and target file
+HOSTS=("pp2" "pp3" "pp4" "pp5")
+SLOTS=1
+
+# My implementation target
+TARGET1="main.cc"
+TARGET2="matrix.cc"
+MAKEFILE_TARGET="Makefile"
+SOURCE_DIR="../src"
+
+DATASET_DIR="../data-set"
+DATASET_TARGET="data2_6"
+DATASET_ANSWER="ans2_6"
+# DATASET_TARGET="data1_1"
+# DATASET_ANSWER="ans1_1"
+
+# Some setting for synchronize different node
+CURRENT_DIR=$(pwd)
+HOME_DIR=$HOME
+SUB_DIR=${CURRENT_DIR#$HOME_DIR/}
+
+# Create a hosts file
+printf "%s\n" "${HOSTS[@]}" > hosts
+# Create a mpi_hosts file with slots
+for i in "${!HOSTS[@]}"; do
+  printf "%s slots=%s\n" "${HOSTS[$i]}" "$SLOTS" >> mpi_hosts
+done
+
+# Compile executable
+cp $SOURCE_DIR/$TARGET1 .
+cp $SOURCE_DIR/$TARGET2 .
+cp $SOURCE_DIR/$MAKEFILE_TARGET .
+cp $DATASET_DIR/$DATASET_TARGET .
+cp $DATASET_DIR/$DATASET_ANSWER .
+make
+
+# Create directories in parallel on each host
+parallel-ssh -h hosts -i "mkdir -p ~/$SUB_DIR"
+
+# Copy the executable to each host in parallel
+parallel-scp -h hosts -r matmul ~/$SUB_DIR
+
+# (Optional) Insert commands here that use the copied files on remote hosts
+mpirun -np 4 --hostfile mpi_hosts matmul < $DATASET_TARGET > result.log
+
+# Extracting the matrix from the output log
+# Assuming the matrix lines do not contain the word 'MPI'
+grep -v 'MPI' result.log > extracted_matrix
+grep 'MPI' result.log > time.log
+
+# Comparing the extracted matrix with the answer file
+diff extracted_matrix $DATASET_ANSWER
+
+# Clean up files on remote hosts in parallel
+parallel-ssh -h hosts -i "rm -rf ~/$SUB_DIR"
+
+# Local clean up
+rm $TARGET1 $TARGET2 $MAKEFILE_TARGET $DATASET_TARGET $DATASET_ANSWER
+rm matmul hosts result.log extracted_matrix
+rm mpi_hosts
+# rm time.log
---
+++ hw4/src/matrix.cc
@@ -0,0 +1,73 @@
+#include <cstdio>
+#include <mpi.h>
+
+int world_rank, world_size;
+
+void construct_matrices(int *n_ptr, int *m_ptr, int *l_ptr,
+                        int **a_mat_ptr, int **b_mat_ptr) {
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    if (world_rank == 0) {
+        scanf("%d %d %d", n_ptr, m_ptr, l_ptr);
+        *a_mat_ptr = new int[*n_ptr * *m_ptr];
+        *b_mat_ptr = new int[*m_ptr * *l_ptr];
+        for(int i = 0; i < *n_ptr * *m_ptr; i++)
+            scanf("%d", *a_mat_ptr + i);
+        for(int i = 0; i < *m_ptr * *l_ptr; i++)
+            scanf("%d", *b_mat_ptr + i);
+    }
+
+    MPI_Bcast(n_ptr, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(m_ptr, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(l_ptr, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (world_rank != 0) {
+        *a_mat_ptr = new int[*n_ptr * *m_ptr];
+        *b_mat_ptr = new int[*m_ptr * *l_ptr];
+    }
+    MPI_Bcast(*a_mat_ptr, (*n_ptr * *m_ptr), MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(*b_mat_ptr, (*m_ptr * *l_ptr), MPI_INT, 0, MPI_COMM_WORLD);
+}
+
+int make_multiple_of_N(int num, int N) {
+    if (num % N == 0) return num;
+    else return ((num / N) + 1) * N;
+}
+
+void matrix_multiply(const int n, const int m, const int l,
+                     const int *a_mat, const int *b_mat) {
+    
+    int pad_n = make_multiple_of_N(n, world_size);
+    int *pad_result = new int[pad_n * l]();
+
+    // for(int i = 0; i < n; i++)
+    //     for(int j = 0; j < l; j++)
+    //         for(int k = 0; k < m; k++)
+    //             pad_result[i * l + j] += a_mat[i * m + k] * b_mat[k * l + j];
+
+
+    int each_row = pad_n / world_size;
+    int each_element = each_row * l;
+    int start_row = world_rank * each_row;
+    int end_row = start_row + each_row;
+    for(int i = start_row; i < end_row; i++)
+        for(int j = 0; j < l; j++)
+            for(int k = 0; k < m; k++)
+                pad_result[i * l + j] += a_mat[i * m + k] * b_mat[k * l + j];
+    
+    MPI_Gather(pad_result + (world_rank * each_element), each_element, MPI_INT, pad_result, each_element, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    if (world_rank == 0) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < l; j++)
+                printf("%d ", pad_result[i * l + j]);
+            printf("\n");
+        }
+    }
+    
+    delete[] pad_result;
+}
+
+void destruct_matrices(int *a_mat, int *b_mat) {
+    delete[] a_mat;
+    delete[] b_mat;
+}
