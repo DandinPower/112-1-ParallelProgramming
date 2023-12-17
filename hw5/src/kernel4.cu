83ac64c2cd3883fc79448a766ea18938e14c43e5
DandinPower <tomhot246@gmail.com>
2023-12-17 22:20:51 +0800

    Refactor kernel4.cu to use group_x_size and group_y_size for thread grouping

--- hw5/src/kernel4.cu
+++ hw5/src/kernel4.cu
@@ -4,6 +4,23 @@
 #include <string.h>
 #include <iostream>
 
+const int group_x_size = 1;
+const int group_y_size = 1;
+
+/**
+ * Allocates device memory using cudaMalloc and checks for errors.
+ *
+ * @param devPtr Pointer to the device memory to be allocated.
+ * @param size   Size of the memory to be allocated.
+ */
+void safe_cuda_malloc(void** devPtr, size_t size) {
+    cudaError_t err = cudaMalloc(devPtr, size);
+    if (err != cudaSuccess) {
+        printf("Error: %s\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+}
+
 /**
  * Calculates the number of iterations required for the given complex number to escape the Mandelbrot set.
  *
@@ -30,19 +47,35 @@ __device__ static inline int mandel(float c_re, float c_im, int count) {
 __global__ void mandelKernel(int *dev_output, float stepX, float stepY, float lowerX, float lowerY, int maxIterations, int pad, int resX, int resY) {
     // To avoid error caused by the floating number, use the following pseudo
     // code
-    int thisX = blockIdx.x * blockDim.x + threadIdx.x;
-    int thisY = blockIdx.y * blockDim.y + threadIdx.y;
+    // int thisX = blockIdx.x * blockDim.x + threadIdx.x;
+    // int thisY = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (thisX >= resX || thisY >= resY) return;
+    int thread_x = (blockIdx.x * blockDim.x + threadIdx.x) * group_x_size;
+    int thread_y = (blockIdx.y * blockDim.y + threadIdx.y) * group_y_size;
 
-    float x = lowerX + thisX * stepX;
-    float y = lowerY + thisY * stepY;
-    int index = thisY * gridDim.x * blockDim.x + thisX;
+    for (int i = 0; i < group_x_size; i++) {
+        for (int j = 0; j < group_y_size; j++) {
+            int thisX = thread_x + i;
+            int thisY = thread_y + j;
 
-    if (index >= resX * resY) return;
+            float x = lowerX + thisX * stepX;
+            float y = lowerY + thisY * stepY;
+            int answer = mandel(x, y, maxIterations);
 
-    index += (index / (gridDim.x * blockDim.x)) * pad;
-    dev_output[index] = mandel(x, y, maxIterations);
+            int index = thisY * gridDim.x * blockDim.x * group_x_size + thisX;
+
+            if (thisX >= resX || thisY >= resY) {
+                continue;
+            }
+            if (index >= resX * resY) {
+                continue;
+            }
+
+            index += (index / resX) * pad;
+            dev_output[index] = answer;
+        }
+
+    }
 }
 
 // Host front-end function that allocates the memory and launches the GPU kernel
@@ -52,24 +85,29 @@ void hostFE(float upperX, float upperY, float lowerX, float lowerY, int* img,
     float stepY = (upperY - lowerY) / resY;
 
     // Allocate memory for the output image
-    // Allocate pinned host memory for the output image
     int* output;
     cudaHostAlloc((void**)&output, resX * resY * sizeof(int), cudaHostAllocDefault);
-
     // Allocate memory on the Cuda device
     int* dev_output;
     size_t pitch;
     cudaMallocPitch((void**)&dev_output, &pitch, resX * sizeof(int), resY);
 
     // define the number of threads per block and the number of blocks
-    dim3 threads_per_block(16, 16);
-    dim3 num_blocks((resX + threads_per_block.x - 1) / threads_per_block.x, (resY + threads_per_block.y - 1) / threads_per_block.y);
+    dim3 threads_per_block(20, 20);
+    dim3 num_blocks(((resX/group_x_size) + threads_per_block.x - 1) / threads_per_block.x, ((resY/group_y_size) + threads_per_block.y - 1) / threads_per_block.y);
 
     // launch the kernel
     mandelKernel<<<num_blocks, threads_per_block>>>(dev_output, stepX, stepY, lowerX, lowerY, maxIterations, (pitch/sizeof(int)) - resX, resX, resY);
 
+    // wait for the kernel to finish
+    // cudaDeviceSynchronize();
+
     // copy the output back to host
     cudaMemcpy2D(output, resX * sizeof(int), dev_output, pitch, resX * sizeof(int), resY, cudaMemcpyDeviceToHost);
+
+    // printf(
+    //     "Total number of iterations: %d\n", output[0]
+    // );
 
     // copy the output to the image
     memcpy(img, output, resX * resY * sizeof(int));
